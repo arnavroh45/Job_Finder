a7c19b8998a3d58f2b5aca2a6b30502b5d8e9fb9
Arnav Sharma <arnavroh45@gmail.com>
2023-02-08 12:11:22 +0530

    changes

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=Path.joinpath(BASE_DIR,'templates')
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
